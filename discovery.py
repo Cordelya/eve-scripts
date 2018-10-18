4c6cf169cfee506043147335c43b68882a068fcf
Jacqueline Arsenault <justcordelya@gmail.com>
2018-10-18 13:01:48 -0400

    Create discovery script
    Created file with list of TODO items to eventually analyze a log file
    for discovered bodies and write a csv file with each body's metadata in
    a row.

---
+++ discovery.py
@@ -0,0 +1,19 @@
+# discovery.py
+# given an EVE log file, write relevant data into a csv file where
+# each row contains the ID of a discovered body, followed by relevant details
+# about the body
+
+# TODO import the json and csv libraries
+
+# TODO import the log file into a dictionary
+
+# TODO initialize the output csv file
+
+# TODO write the csv headers line
+
+# TODO store the "Discovered" data subset into a new list
+
+# TODO iterate over the discovered list and write the body name followed by
+# each attribute, separated by commas and ending with a semi-colon
+
+# TODO close the csv file
